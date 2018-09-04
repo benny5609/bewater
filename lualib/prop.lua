0096145f6b80232d5d0041e22329bfd1bd9fb6ff
huangjx <zhandouxiaojiji@gmail.com>
2018-09-04 09:46:52 +0800

    转换json表中的number类型

--- lualib/prop.lua
+++ lualib/prop.lua
@@ -1,6 +1,7 @@
 local conf = require "conf"
 local json = require "cjson"
 local const = require "const"
+local util = require "util"
 
 local M = {}
 local props = {}
@@ -11,7 +12,7 @@ function M.json(name)
     end
     local file = io.open(conf.workspace.."/script/def/prop/"..name..".json", "r")
     local str = file:read("*a")
-    local prop = const(json.decode(str))
+    local prop = const(util.str2num(json.decode(str)))
     props[name] = prop
     return prop
 end
