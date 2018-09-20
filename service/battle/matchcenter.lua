83bc735e1ea55f4a009014acb71193e1e70036d3
huangjx <zhandouxiaojiji@gmail.com>
2018-09-20 20:00:41 +0800

    取消匹配

--- service/battle/matchcenter.lua
+++ service/battle/matchcenter.lua
@@ -15,6 +15,11 @@ function CMD.match(mode, uid, value, agent)
     skynet.call(addr, "lua", "start", uid, value, agent)
 end
 
+function CMD.cancel(mode, uid)
+    local addr = assert(addrs[mode])
+    skynet.call(addr, "lua", "cancel", uid)
+end
+
 skynet.start(function()
     skynet.dispatch("lua", function(_, _, cmd, ...)
         local f = assert(CMD[cmd], cmd)
