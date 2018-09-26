90b2ff21b8c5e800a3d7bba8e2227c61bfa6071c
huangjx <zhandouxiaojiji@gmail.com>
2018-09-26 22:20:17 +0800

    一分钟发一次警报

--- service/alert.lua
+++ service/alert.lua
@@ -1,11 +1,13 @@
 -- 钉钉警报系统
 local skynet = require "skynet"
-local http = require "web.http_helper"
-local util = require "util"
-local conf = require "conf"
-local log = require "log"
-local json = require "cjson"
-local trace = log.trace("alert")
+local http   = require "web.http_helper"
+local util   = require "util"
+local conf   = require "conf"
+local log    = require "log"
+local json   = require "cjson"
+
+local print  = log.print("print")
+local trace  = log.trace("alert")
 require "bash"
 
 local host = "https://oapi.dingtalk.com"
@@ -19,23 +21,39 @@ local function get_token()
     end 
 end
 
-local CMD = {}
-function CMD.traceback(err)
-    --[[local ret, resp = http.post(host.."/message/send_to_conversation?access_token="..token, json.encode {
-        sender = "manager3375",
-        cid ="dcb66a48183e3cfe8fbce9207c3ecec9",
-        msgtype = "text",
-        text = { 
-            content = str
-        }   
-    })
-    print(ret, resp)]]
+
+local count = 0 -- 一分钟内累计报错次数
+local last = 0  -- 上次报错时间
+local function send_traceback()
     local info = require "clusterinfo"
     local path = string.format("%s/log/%s.log", info.workspace, skynet.getenv("clustername") or "error")
-    local str = string.format("服务器TRACEBACK\n项目:%s\n节点:%s\n公网ip:%s\n内网ip:%s\n进程:%s\n路径:%s\n自己上去看log，傻逼!",
-        conf.desc or conf.proj, info.clustername, info.pnet_addr, info.inet_addr, info.pid, path)
-    --print(str)
-    CMD.test(str) 
+    local str = string.format("服务器TRACEBACK\n项目:%s\n节点:%s\n公网ip:%s\n内网ip:%s\n进程:%s\n路径:%s\n累计报错:%d次",
+        conf.desc or conf.proj, info.clustername, info.pnet_addr, info.inet_addr, info.pid, path, count)
+
+    count = 0
+    last = os.time()
+
+    local token = get_token()
+    local sh = string.format('curl -H "Content-Type:application/json" -X POST -d \'%s\' %s/chat/send?access_token=%s', json.encode {
+        sender = conf.alert.sender,
+        chatid = conf.alert.chatid,
+        msgtype = "text",
+        text = { 
+            content = str,
+        } 
+    }, host, token)
+    print(sh)
+    bash(sh)
+    
+end
+
+local CMD = {}
+function CMD.traceback(err)
+    count = count + 1
+    if os.time() - last < 60 then
+        return
+    end
+    send_traceback() 
 end
 
 function CMD.node_dead(proj, clustername, pnet_addr, inet_addr, pid, cpu, mem)
@@ -65,5 +83,14 @@ skynet.start(function()
     skynet.dispatch("lua", function(_,_, cmd, ...)
         local f = assert(CMD[cmd], cmd)
         util.ret(f(...))
+    end)
+
+    skynet.fork(function()
+        while true do
+            if count > 0 then
+                send_traceback()
+            end
+            skynet.sleep(6000)
+        end
     end)
 end)
