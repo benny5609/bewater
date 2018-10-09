a404605a0dc5b8e534b466f4d507080ccd54a558
huangjx <zhandouxiaojiji@gmail.com>
2018-10-09 22:04:59 +0800

    bugfix

--- lualib/def/errcode_helper.lua
+++ lualib/def/errcode_helper.lua
@@ -17,15 +17,30 @@ function errcode.describe(code)
     return code2describe[code]
 end
 
+function errcode.get_name2errcode()
+    return name2errcode
+end
+
 setmetatable(errcode, {__index = function (_, name)
     return assert(name2errcode[name], name)
 end})
 
 REG("OK",                   0x0000, "执行成功")
-REG("TRACEBACK",            0x0001, "服务器报错！")
-REG("AUTH_FAIL",            0x0002, "验证错误!")
-REG("SIGN_ERROR",           0x0003, "签名错误")
-REG("SERVER_BUSY",          0x0004, "服务器忙！")
-REG("SERVER_CLOSED",        0x0005, "服役器未启动！")
+REG("TRACEBACK",            0x0001, "服务器报错!")
+REG("TODO",                 0x0002, "功能开发中")
+REG("ARGS_ERROR",           0x0003, "参数错误")
+REG("AUTH_FAIL",            0x0004, "验证错误!")
+REG("OFFLINE",              0x0005, "已下线")
+REG("PROP_ERROR",           0x0006, "配置表错误")
+REG("RECONNECTED",          0x0007, "重连成功")
+REG("RELOGIN",              0x0008, "重登成功")
+REG("KICK",                 0x0009, "在别处登陆")
+REG("SERVER_STOP",          0x000a, "服务器维护中")
+REG("VERSION_TOO_LOW",      0x000b, "版本更新中")
+REG("ACTIVITY_NOT_OPEN",    0x000c, "活动未开放")
+REG("REPEAT",               0x000d, "重复操作")
+REG("SIGN_ERROR",           0x000e, "签名错误")
+REG("SERVER_BUSY",          0x000f, "服务器忙！")
+REG("SERVER_CLOSED",        0x0010, "服役器未启动！")
 
 return errcode
