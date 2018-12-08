feea024a001ab391903286dc366e9c40a45ea76e
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2018-12-09 00:16:02 +0800

    cms menu

--- lualib/cms/server.lua
+++ lualib/cms/server.lua
@@ -2,11 +2,15 @@ local skynet = require "skynet"
 local errcode = require "def.errcode"
 local uuid = require "uuid"
 local log = require "log"
+local util = require "util"
 
 local trace = log.trace("cms")
 
 local M = {}
-local menu = {}
+local menu = {
+    left = {},  -- 左侧菜单
+    top = {},   -- 顶部菜单
+}
 local acc2info = {}
 local auth2acc = {}
 function M.start(users)
@@ -15,6 +19,28 @@ function M.start(users)
     end
 
     skynet.register "cms"
+end
+
+function M.add_menu(side, new, ...)
+    local item_list = assert(menu[side], side)
+    local idx_list = {...}
+    if #idx_list == 0 then
+        table.insert(item_list, new)
+    else
+        local cur = item_list
+        for _, idx in ipairs(idx_list) do
+            cur = cur[idx] 
+        end
+        table.insert(cur, new)
+    end
+end
+
+function M.insert_left_menu(menu, ...)
+    M.add_menu("left", menu, ...)
+end
+
+function M.insert_top_menu(menu, ...)
+    M.add_menu("top", menu, ...)
 end
 
 function M.get_account(auth)
@@ -45,6 +71,7 @@ function M.req_login(account, password)
 end
 
 function M.req_menu()
+    util.printdump(menu)
     return menu
 end
 return M
--- lualib/cms/webconsole.lua
+++ lualib/cms/webconsole.lua
@@ -8,9 +8,20 @@ function M.init(args)
     local users = assert(args.users)
     web = skynet.newservice("web/webserver", "gate", "cms.server", "cms.handler", port, 10)
     skynet.call(web, "lua", "start", users)
+
+    skynet.call(web, "lua", "insert_left_menu", {
+        text = "Skynet", children = {
+            {text = "节点信息", view = "view/node_info"},
+            {text = "所有服务", view = "view/all_service"},
+            {text = "注入调试", view = "view/inject"},
+            {text = "GM", view = "view/gm"},
+        }
+    })
 end
+
 function M.add_api(path)
     assert(web, "webconsole not init")
     skynet.call(web, "lua", "add_api", path)
 end
+
 return M
