1039b797b5a8d269cb996c2819cde276998b6c67
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2019-05-16 02:54:03 +0800

    update protobuf

--- lualib/bw/protobuf.lua
+++ lualib/bw/protobuf.lua
@@ -7,9 +7,11 @@ local assert = assert
 local pairs = pairs
 local ipairs = ipairs
 local string = string
+local print = print
 local io = io
 local tinsert = table.insert
 local rawget = rawget
+local rawset = rawset
 
 local M = {}
 
@@ -31,11 +33,6 @@ M.GC = GC
 function M.lasterror()
 	return c._last_error(P)
 end
-
-local map_entry_cache = setmetatable({}, {__index = function (self, typename)
-	rawset(self, typename, c._env_map_entry(P, typename))
-	return self[typename]
-end})
 
 local decode_type_cache = {}
 local _R_meta = {}
@@ -147,11 +144,11 @@ end
 #define PBC_REPEATED 128
 ]]
 
-_reader[1] = function() return _reader.int end
-_reader[2] = function() return _reader.real end
-_reader[3] = function() return _reader.bool end
-_reader[4] = function() return _reader.string end
-_reader[5] = function() return _reader.string end
+_reader[1] = function(msg) return _reader.int end
+_reader[2] = function(msg) return _reader.real end
+_reader[3] = function(msg) return _reader.bool end
+_reader[4] = function(msg) return _reader.string end
+_reader[5] = function(msg) return _reader.string end
 _reader[6] = function(msg)
 	local message = _reader.message
 	return	function(self,key)
@@ -164,11 +161,11 @@ _reader[9] = _reader[5]
 _reader[10] = _reader[7]
 _reader[11] = _reader[7]
 
-_reader[128+1] = function() return _reader.int_repeated end
-_reader[128+2] = function() return _reader.real_repeated end
-_reader[128+3] = function() return _reader.bool_repeated end
-_reader[128+4] = function() return _reader.string_repeated end
-_reader[128+5] = function() return _reader.string_repeated end
+_reader[128+1] = function(msg) return _reader.int_repeated end
+_reader[128+2] = function(msg) return _reader.real_repeated end
+_reader[128+3] = function(msg) return _reader.bool_repeated end
+_reader[128+4] = function(msg) return _reader.string_repeated end
+_reader[128+5] = function(msg) return _reader.string_repeated end
 _reader[128+6] = function(msg)
 	local message = _reader.message_repeated
 	return	function(self,key)
@@ -215,9 +212,9 @@ end
 local encode_type_cache = {}
 
 local function encode_message(CObj, message_type, t)
-	local tt = encode_type_cache[message_type]
+	local type = encode_type_cache[message_type]
 	for k,v in pairs(t) do
-		local func = tt[k]
+		local func = type[k]
 		func(CObj, k , v)
 	end
 end
@@ -257,16 +254,9 @@ function _writer:string_repeated(k,v)
 end
 
 function _writer:message_repeated(k,v, message_type)
-	if map_entry_cache[message_type] then
-		for dk , dv in pairs(v) do
-			local submessage = c._wmessage_message(self, k)
-			encode_message(submessage, message_type, {key=dk, value=dv})
-		end
-	else
-		for _ ,v in ipairs(v) do
-			local submessage = c._wmessage_message(self, k)
-			encode_message(submessage, message_type, v)
-		end
+	for _,v in ipairs(v) do
+		local submessage = c._wmessage_message(self, k)
+		encode_message(submessage, message_type, v)
 	end
 end
 
@@ -276,11 +266,11 @@ function _writer:int_repeated(k,v)
 	end
 end
 
-_writer[1] = function() return _writer.int end
-_writer[2] = function() return _writer.real end
-_writer[3] = function() return _writer.bool end
-_writer[4] = function() return _writer.string end
-_writer[5] = function() return _writer.string end
+_writer[1] = function(msg) return _writer.int end
+_writer[2] = function(msg) return _writer.real end
+_writer[3] = function(msg) return _writer.bool end
+_writer[4] = function(msg) return _writer.string end
+_writer[5] = function(msg) return _writer.string end
 _writer[6] = function(msg)
 	local message = _writer.message
 	return	function(self,key , v)
@@ -293,11 +283,11 @@ _writer[9] = _writer[5]
 _writer[10] = _writer[7]
 _writer[11] = _writer[7]
 
-_writer[128+1] = function() return _writer.int_repeated end
-_writer[128+2] = function() return _writer.real_repeated end
-_writer[128+3] = function() return _writer.bool_repeated end
-_writer[128+4] = function() return _writer.string_repeated end
-_writer[128+5] = function() return _writer.string_repeated end
+_writer[128+1] = function(msg) return _writer.int_repeated end
+_writer[128+2] = function(msg) return _writer.real_repeated end
+_writer[128+3] = function(msg) return _writer.bool_repeated end
+_writer[128+4] = function(msg) return _writer.string_repeated end
+_writer[128+5] = function(msg) return _writer.string_repeated end
 _writer[128+6] = function(msg)
 	local message = _writer.message_repeated
 	return	function(self,key, v)
@@ -438,7 +428,18 @@ local function default_table(typename)
 		return v
 	end
 
-	v = { __index = assert(decode_message(typename , "")) }
+	local default_inst = assert(decode_message(typename , ""))
+	v = { 
+		__index = function(tb, key)
+			local ret = default_inst[key]
+			if 'table' ~= type(ret) then
+				return ret
+			end 
+			ret = setmetatable({}, { __index = ret })
+			rawset(tb, key, ret)
+			return ret
+		end
+	}
 
 	default_cache[typename]  = v
 	return v
