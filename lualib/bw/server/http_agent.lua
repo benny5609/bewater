94743f11ebe2a3ff7651a16e13b31a26ed79e44f
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2019-05-20 18:53:42 +0800

    change http_agent

--- lualib/bw/server/http_agent.lua
+++ lualib/bw/server/http_agent.lua
@@ -3,11 +3,31 @@ local socket        = require "skynet.socket"
 local httpd         = require "http.httpd"
 local sockethelper  = require "http.sockethelper"
 local urllib        = require "http.url"
+local json          = require "cjson.safe"
 local bewater       = require "bw.bewater"
 local util          = require "bw.util"
 local errcode       = require "def.errcode"
---local log           = require "bw.log"
---local print = log.print("agentserver")
+local log           = require "bw.log"
+local print = log.print("agentserver")
+
+local api = {}
+
+local function default_pack(ret)
+    if type(ret) == "table" then
+        ret.err = ret.err or 0
+        return json.encode(ret)
+    else
+        return json.encode({err = ret})
+    end
+end
+
+local function default_unpack(str)
+    return json.decode(str)
+end
+
+local function default_auth(p)
+    error("auth function not provide")
+end
 
 local function response(fd, ...)
     local writefunc = sockethelper.writefunc(fd)
@@ -18,71 +38,56 @@ local function response(fd, ...)
     end
 end
 
+local function on_message(url, args, body, header, ip)
+    local authorization = header.authorization
+    local process = api[url]
+    if process then
+        local pack   = process.pack or default_pack
+        local unpack = process.unpack or default_unpack
+        local auth   = process.auth and default_auth
 
-local function on_message(handler, url, args, body, header, ip)
-    local auth = header.authorization
-    local api = handler.api[url]
-    if api then
+        local function errf(err, fmt, ...)
+            return pack {
+                err = err,
+                desc = string.format(fmt, ...),
+                url = url
+            }
+        end
+
         local ret, data = bewater.try(function()
-            skynet.error("######", body)
-            return handler.unpack(body, url)
+            return unpack(body, url)
         end)
         if not ret then
-            return {
-                err = errcode.BODY_ERROR,
-                desc = "body error",
-                url = url,
-            }
+            return errf(errcode.BODY_ERROR, "body error")
         end
         ret = {}
-        local uid = handler.auth and handler.auth(auth)
-        if api.auth and not uid then
-            return {
-                err = errcode.AUTH_FAIL,
-                desc = "authorization fail",
-                url = url,
-            }
-        end
-        if api.data then
-            if not data then
-                return {
-                    err = errcode.ARGS_ERROR,
-                    desc = "data nil",
-                    url = url,
-                }
+        local uid = process.auth
+        if process.auth and auth then
+            uid = auth(authorization)
+            if not uid then
+                return errf(errcode.AUTH_FAIL, "authorization fail")
             end
-            for k, t in pairs(api.data) do
+        end
+        if process.data then
+            if not data then
+                return errf(errcode.ARGS_ERROR, "data nil")
+            end
+            for k, t in pairs(process.data) do
                 if t == "str" then
                     if type(data[k]) ~= "string" then
-                        return {
-                            err = errcode.ARGS_ERROR,
-                            desc = string.format("args error, %s must string", k),
-                            url = url,
-                        }
+                        return errf(errcode.ARGS_ERROR, "args error, %s must string", k)
                     end
                 elseif t == "str?" then
                     if data[k] and type(data[k]) ~= "string" then
-                        return {
-                            err = errcode.ARGS_ERROR,
-                            desc = string.format("args error, %s must string", k),
-                            url = url,
-                        }
+                        return errf(errcode.ARGS_ERROR, "args error, %s must string", k)
                     end
                 elseif t == "num" then
                     if type(data[k]) ~= "number" then
-                        return {
-                            err = errcode.ARGS_ERROR,
-                            desc = string.format("args error, %s must number", k),
-                            url = url,
-                        }
+                        return errf(errcode.ARGS_ERROR, "args error, %s must number", k)
                     end
                 elseif t == "num?" then
                     if data[k] and type(data[k]) ~= "number" then
-                        return {
-                            err = errcode.ARGS_ERROR,
-                            desc = string.format("args error, %s must number", k),
-                            url = url,
-                        }
+                        return errf(errcode.ARGS_ERROR, "args error, %s must number", k)
                     end
                 else
                     util.printdump(data)
@@ -91,9 +96,8 @@ local function on_message(handler, url, args, body, header, ip)
             end
         end
         if not bewater.try(function()
-            local func = require(handler.topath(url))
-            assert(func, url)
-            ret = func(args, data, uid, ip, header) or {}
+            local func = process.handler
+            ret = process.handler(args, data, uid, ip, header) or {}
             if type(ret) == "number" then
                 ret = {err = ret}
                 if ret.err ~= 0 then
@@ -102,22 +106,17 @@ local function on_message(handler, url, args, body, header, ip)
                 end
             end
         end) then
-            return {
-                err = errcode.TRACEBACK,
-                desc = "server traceback",
-                url = url,
-            }
+            return errf(errcode.TRACEBACK, "server traceback")
         end
         if type(ret) == "table" then
             ret.err = ret.err or 0
         end
-        return ret
+        return pack(ret)
     else
-        return {
+        return default_pack({
             err = errcode.API_NOT_EXIST,
-            desc = "api not exist",
-            url = url,
-        }
+            desc = "api not exist"
+        })
     end
 end
 
@@ -131,21 +130,14 @@ local function resp_options(fd, header)
     socket.close(fd)
 end
 
-local agentserver = {}
-function agentserver.start(handler)
+local M = {}
+function M.start(handler)
     -- handler 需要提供
-    -- handler.api = {[[/api/xxx/ooo]] = func}
     -- handler.auth = function(auth) return uid end -- 授权
     -- 如果是非字符串，handler需要提供pack和unpack方法
-    handler.pack = handler.pack or function (data)
-        return data
-    end
-    handler.unpack = handler.unpack or function (data)
-        return data
-    end
-    handler.topath = handler.topath or function(url)
-        return string.sub(url, 2, -1)
-    end
+    default_pack = handler.pack or default_pack
+    default_unpack = handler.unpack or default_unpack
+    default_auth = handler.auth or default_auth
 
     skynet.start(function()
         skynet.dispatch("lua", function (_,_, ...)
@@ -174,7 +166,7 @@ function agentserver.start(handler)
                         data = urllib.parse_query(query)
                     end
                     ip = header['x-real-ip'] or string.match(ip, "[^:]+")
-                    response(fd, code, handler.pack(on_message(handler, url, data, body, header, ip)),
+                    response(fd, code, on_message(url, data, body, header, ip),
                     {
                         ['Access-Control-Allow-Origin'] = header['origin'],
                         ['Access-Control-Allow-Methons'] = 'PUT, POST, GET, OPTIONS, DELETE',
@@ -193,4 +185,16 @@ function agentserver.start(handler)
         end)
     end)
 end
-return agentserver
+
+function M.reg(params)
+    api[params.url] = {
+        url     = assert(params.url),
+        handler = assert(params.handler),
+        pack    = params.pack,
+        unpack  = params.unpack,
+        auth    = params.auth,
+        data    = params.data,
+    }
+end
+
+return M
